c242a8185b02721670e5842b43b3eb9899da57ca
asahiimura <58813834+asahiimura@users.noreply.github.com>
2019-12-30 15:22:31 +0900

    messages

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,12 +20,12 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = group.find(params[:id])
+    # @group = group.find(params[:id])
   end
 
   def update
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group) , notice: 'グループを更新しました'
     else
       render :edit
     end
